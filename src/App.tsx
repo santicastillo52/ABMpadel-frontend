f17d262513a1c0545700c965b3a3f72e15003a70
santi <santicastillo52@gmail.com>
2025-09-03 18:54:26 -0300

    Creacion de header con algunos estilos y ruteo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Header } from './components/Header/Header'
 import { Dashboard } from "./layouts/Dashboard";
+import { CourtGestor } from "./features/Courts/Court-gestor/CourtGestor";
+import { Names } from "./components/Names/Names";
 
 export const App: React.FC = () => {
   return (
-    <>
-      <Dashboard />
-    </>
+    <div>
+      <Header/>
+      <main>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}></Route>
+          <Route path="/court-gestor" element={<CourtGestor/>}></Route>
+          <Route path="/names" element={<Names/>}></Route>
+
+        </Routes>
+      </main>
+    </div>
   );
 };
 
--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CourtsContainer } from '../features/Courts/Courts-Container/CourtsContainer'
 
+
 export const Dashboard: React.FC = () => {
   return (
     <div>
